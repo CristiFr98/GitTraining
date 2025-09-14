38a72fe4c9ad641784ae7401596163c40cc84ecb
Cristi Fratiman <cristi.fratiman@gmail.com>
2025-09-14 13:36:01 +0300

    Initial Commit

---
+++ GitTraining/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitTraining
+//
+//  Created by Cristi on 14.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "heart.fill")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitTraining/GitTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrainingApp.swift
+//  GitTraining
+//
+//  Created by Cristi on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GitTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
