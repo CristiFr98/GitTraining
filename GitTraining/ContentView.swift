bb6ecaa30a6f79effb61240c6b12443da60eee91
Cristi Fratiman <cristi.fratiman@gmail.com>
2025-09-14 20:39:22 +0300

    [Feature] Changed color to red

--- GitTraining/ContentView.swift
+++ GitTraining/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "heart.fill")
+                .foregroundColor(Color.red)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
